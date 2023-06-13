36972c100f37f213c17ad6d26c679a629543d927
NotEli809 <nomadkillz007@gmail.com>
2023-06-13 03:49:44 -0400

    added basic UI design for main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,15 @@ void main() {
   ));
 }
 
-class OurosHome extends StatelessWidget {
+class OurosHome extends StatefulWidget {
   const OurosHome({Key? key}) : super(key: key);
 
+  @override
+  State<OurosHome> createState() => _OurosHomeState();
+}
+const name = "XD";
+
+class _OurosHomeState extends State<OurosHome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,8 +24,35 @@ class OurosHome extends StatelessWidget {
         ),
         backgroundColor: Colors.black,
       ),
-      body: Center(
-
+      body: Column(
+        children: [
+          SizedBox(height: 10),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(10,0,0,20),
+                child: Text("Welcome $name"),
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Checkbox(value: false, onChanged: null),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0,0,196,0),
+                child: Text("Do this"),
+              ),
+              IconButton(onPressed: () {}, icon: Icon(
+                Icons.delete
+              )),
+              IconButton(onPressed: () {}, icon: Icon(
+                  Icons.edit
+              ))
+            ],
+          )
+        ],
       ),
     );
   }
